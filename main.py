3cb47842021ad14fc557600642391e0e5f2bd6d0
JeremyS-T <jtanrev@gmail.com>
2024-01-18 14:01:12 +0800

    Initial Commit.

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, redirect, url_for, request
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
+
+
+@app.route('/')
+def main_page():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
