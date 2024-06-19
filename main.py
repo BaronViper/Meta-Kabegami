46785eb0f83b8dd28ea8b5ce76a7f13fce398eba
JeremyTan0 <tanjer@oregonstate.edu>
2024-06-19 12:02:26 +0800

    Implement image wallpaper generator functionality

--- main.py
+++ main.py
@@ -40,20 +40,27 @@ def main_page():
         img_address = request.form['pname']
         driver.get(url=img_address)
         try:
-            element = WebDriverWait(driver, 10).until(
+            element = WebDriverWait(driver, 20).until(
                 EC.presence_of_element_located((By.CLASS_NAME, "Image--image"))
             )
+
+            # Get img url
             img = driver.find_element(By.CLASS_NAME, 'Image--image')
             img_src = img.get_attribute('src')
-            urllib.request.urlretrieve(url=img_src, filename="target_img")
+
+            urllib.request.urlretrieve(url=img_src, filename="target_img.png")
 
             # Convert image to wallpaper (3840x1818)
-            img = Image.open("target_img")
-            ImageOps.fit(img, (3840, 1818)).save("target_img")
+            img = Image.open("target_img.png")
+            pix = img.load()
+            palette_rgb = pix[1, 1]
+            resized_img = ImageOps.fit(img, size=(1818, 1818))
+            ImageOps.pad(resized_img, (1818, 3840), color=palette_rgb, centering=(0.5, 1)).save("static/images/image_converted.png")
 
             return render_template('create.html', img_src=img_src)
         except TimeoutException:
             print("Timed out waiting for page to load")
+            return render_template('index.html')
 
 
 @app.route('/create', methods=["POST", "GET"])
