3623c3959477e14443f5aa049ac84ffbc32fbf7a
JeremyTan0 <tanjer@oregonstate.edu>
2024-06-19 14:12:12 +0800

    Add base layout to final output page.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, session
+from flask_session import Session
 from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.common import TimeoutException
@@ -8,6 +9,7 @@ from fake_headers import Headers
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 import urllib.request
+import time
 from PIL import Image, ImageOps
 import os
 
@@ -15,7 +17,6 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 load_dotenv()
 chrome_driver_path = "C:\Development\chromedriver.exe"
-options = webdriver.ChromeOptions()
 
 header = Headers(
     browser="chrome",  # Generate only Chrome UA
@@ -23,30 +24,30 @@ header = Headers(
     headers=False # generate misc headers
 )
 customUserAgent = header.generate()['User-Agent']
-
-options.add_argument(f"user-agent={customUserAgent}")
-options.add_argument('--headless')
-driver = webdriver.Chrome(service=Service(chrome_driver_path), options=options)
+# options = webdriver.ChromeOptions()
+# options.add_argument(f"user-agent={customUserAgent}")
+# options.add_argument('--headless')
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 
-
 @app.route('/', methods=["POST", "GET"])
 def main_page():
+    driver = webdriver.Chrome(service=Service(chrome_driver_path))
+    # Set up driver
     if request.method == "GET":
         return render_template('index.html')
     else:
         img_address = request.form['pname']
         driver.get(url=img_address)
         try:
-            element = WebDriverWait(driver, 20).until(
+            img_src = WebDriverWait(driver, 10).until(
                 EC.presence_of_element_located((By.CLASS_NAME, "Image--image"))
-            )
+            ).get_attribute('src')
 
-            # Get img url
-            img = driver.find_element(By.CLASS_NAME, 'Image--image')
-            img_src = img.get_attribute('src')
+            title = driver.find_element(By.CLASS_NAME, "item--title").get_attribute("innerHTML")
+
+            driver.quit()
 
             urllib.request.urlretrieve(url=img_src, filename="target_img.png")
 
@@ -57,16 +58,26 @@ def main_page():
             resized_img = ImageOps.fit(img, size=(1818, 1818))
             ImageOps.pad(resized_img, (1818, 3840), color=palette_rgb, centering=(0.5, 1)).save("static/images/image_converted.png")
 
-            return render_template('create.html', img_src=img_src)
+            session['title'] = title
+            session['img_src'] = img_src
+
+            return redirect('/create')
         except TimeoutException:
             print("Timed out waiting for page to load")
+            driver.quit()
             return render_template('index.html')
 
 
 @app.route('/create', methods=["POST", "GET"])
 def create_page():
     if request.method == "GET":
-        return render_template('create.html', img_src="static/images/test.png")
+        return render_template('create.html', img_src=session['img_src'], art_title=session['title'])
+
+
+@app.route('/create/test', methods=["POST", "GET"])
+def create_page():
+    if request.method == "GET":
+        return render_template('create.html', img_src=r"https://i.seadn.io/s/raw/files/0d304ef87f29306ae752b241871ec0d3.png?auto=format&dpr=1&w=1000", art_title="Azuki #5334")
 
 
 if __name__ == '__main__':
