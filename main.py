9a1545570b95aaac4e3df722252ad04e5bcf3e88
BaronViper <jtanrev@gmail.com>
2025-02-13 23:32:49 +0800

    Utilize Rarible for NFT retrieval

--- main.py
+++ main.py
@@ -7,24 +7,17 @@ from curl_cffi import requests
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
-import cv2
+from io import BytesIO
 import urllib.request
 import random
 import smtplib
 import os
 
-
 load_dotenv()
-user_agents = [
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-    'Mozilla/5.0 (Macintosh;     Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15',
-    'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0',
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0'
-]
-
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
+app.secret_key = os.urandom(24)
 
 @app.route('/', methods=["POST", "GET"])
 def main_page():
@@ -32,42 +25,23 @@ def main_page():
     if request.method == "GET":
         return render_template('index.html')
     else:
-        user_agent = random.choice(user_agents)
-        headers = {
-            'User-Agent': user_agent,
-            'Accept-Language': 'en-US,en;q=0.9',
-            'Connection': 'keep-alive',
-            'Upgrade-Insecure-Requests': '1',
-            'Cache-Control': 'max-age=0',
-            'Sec-Fetch-Dest': 'document',
-            'Sec-Fetch-Mode': 'navigate',
-            'Sec-Fetch-Site': 'none',
-            'Sec-Fetch-User': '?1',
-            'Sec-GPC': '1',
-            'Referer': 'https://www.google.com/'
-        }
-
-        response = requests.get(
-            "https://api.proxyscrape.com/v3/free-proxy-list/get?request=displayproxies&proxy_format=protocolipport&format=text&protocol=http")
-        proxy_list = response.content.decode().split('\n')
-        proxy_list = [proxy.strip() for proxy in proxy_list if proxy.strip()]
-        proxy = random.choice(proxy_list)
-        proxies = {
-            'http': proxy
-        }
         img_address = request.form['pname']
-        response = requests.get(img_address, headers=headers, proxies=proxies)
+        response = requests.get(img_address)
         html_content = response.text
         soup = BeautifulSoup(html_content, 'html.parser')
         try:
             if response.status_code == 200:
-                img_src = soup.find("img", class_="Image--image")['src']
-                title = soup.find("h1", class_="item--title").get_text()
+                img_tag = soup.find("meta", property="og:image")
+                title_tag = soup.find("meta", property="og:title")
+                if img_tag and title_tag:
+                    img_src = img_tag["content"]
+                    title = title_tag["content"]
 
-                urllib.request.urlretrieve(url=img_src, filename="target_img.png")
 
-                # Convert image to wallpaper (3840x1818)
-                img = Image.open("target_img.png")
+                response = requests.get(img_src)
+
+                img = Image.open(BytesIO(response.content))
+                img.save("target_img.png")
                 pix = img.load()
                 palette_rgb = pix[1, 1]
                 resized_img = ImageOps.fit(img, size=(1818, 1818))
